10697eba425b329ef424b2f5238b9c39f219b7ce
hugepizza <wangleilei950325@gmail.com>
2024-01-16 18:14:04 +0800

    users

--- src/components/pages/Index.tsx
+++ src/components/pages/Index.tsx
@@ -2,7 +2,21 @@ import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import AnimatedNumbers from "react-animated-numbers";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 function Index() {
+  const [users, setUsers] = useState(0);
+  useEffect(() => {
+    fetch("https://worker.vocatest.online/", {
+      method: "GET",
+      headers: { "Content-Type": "text/plain", "x-signal": "get" },
+    })
+      .then((resp) => resp.text())
+      .then((cstr) => parseInt(cstr, 10) || 0)
+      .then((c) => setUsers(c))
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div className="w-full h-full">
       <div className="w-full h-1/2 flex flex-col items-center space-y-1">
@@ -18,7 +32,18 @@ function Index() {
       <div className="w-full h-1/2 flex flex-col items-center space-y-1">
         <div className="h-2/5"></div>
         <Link to={"/test"}>
-          <Button size={"lg"}>开始</Button>
+          <Button
+            size={"lg"}
+            onClick={() => {
+              fetch("https://worker.vocatest.online/", {
+                method: "GET",
+              }).catch((err) => {
+                console.log(err);
+              });
+            }}
+          >
+            开始
+          </Button>
         </Link>
         <div className="h-2"></div>
         <div className="flex flex-row  space-x-1">
@@ -29,7 +54,7 @@ function Index() {
                 type: "spring",
                 duration: index + 0.1,
               })}
-              animateToNumber={411}
+              animateToNumber={users}
             />
           </Badge>
           <span>人参与过此测试</span>
